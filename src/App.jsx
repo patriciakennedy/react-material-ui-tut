1d110be045058917afed0e0212ee1fc23661b65b
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-02-11 20:09:55 -0600

    Added Navbar with MUI AppBar, Toolbar, and styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from './components/Navbar';
 import Feed from './components/Feed';
 import Rightbar from './components/Rightbar';
 import Sidebar from './components/Sidebar';
-import { Box, Container, Stack } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 
 function App() {
     return (
+        
         <Box>
-            <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Navbar/>
+          <Stack direction="row" spacing={2} justifyContent="space-between">
                 <Sidebar />
                 <Feed />
                 <Rightbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import { Box } from '@mui/material';
+import { AppBar, styled, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 
-function Navbar() {
-    return <Box bgcolor="skyblue">Sidebar</Box>;
-}
+const StyledToolbar = styled(Toolbar)({
+    display: 'flex',
+    justifyContent: 'space-between',
+});
+
+const Navbar = () => {
+    return (
+        <AppBar position="sticky">
+            <StyledToolbar>
+                <Typography>CodeChick</Typography>
+            </StyledToolbar>
+        </AppBar>
+    );
+};
 
 export default Navbar;
