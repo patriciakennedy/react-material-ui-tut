697169a7db7ffd53c978437d8503979e2e73e435
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-02-11 19:28:08 -0600

    Styled Rightbar component with MUI props and responsive display

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,5 +2,5 @@ import { Box } from '@mui/material';
 import React from 'react';
 
 export default function Feed() {
-    return <Box bgcolor="pink" flex={4}>Feed</Box>;
+    return <Box bgcolor="pink" flex={4} p={2}>Feed</Box>;
 }
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -2,7 +2,16 @@ import { Box } from '@mui/material';
 import React from 'react';
 
 function Rightbar() {
-    return <Box bgcolor="lightcoral" flex={2}>Rightbar</Box>;
+    return (
+        <Box
+            bgcolor="lightcoral"
+            flex={2}
+            p={2}
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+        >
+            Rightbar
+        </Box>
+    );
 }
 
 export default Rightbar;
