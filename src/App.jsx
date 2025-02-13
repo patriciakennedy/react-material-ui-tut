235c2df27e9b9bbc5e8f3e787f76215f694dfa6b
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-02-12 22:35:44 -0600

    Rename Buttons.jsx to Typesbuttons.jsx and updated its content

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,26 @@ import Feed from './components/Feed';
 import Rightbar from './components/Rightbar';
 import Sidebar from './components/Sidebar';
 import { Box, Button, Stack } from '@mui/material';
-
+import Typebuttons from './components/Sidebar';
 
 function App() {
     return (
-        <Box>
-            <Navbar />
-            <Button />
+        <div>
+            <Box>
+                <Navbar />
 
-            <Stack direction="row" spacing={2} justifyContent="space-between">
-                <Sidebar />
-                <Feed />
-                <Rightbar />
-            </Stack>
-        </Box>
+                <Stack
+                    direction="row"
+                    spacing={2}
+                    justifyContent="space-between"
+                >
+                    <Sidebar />
+                    <Feed />
+                    <Rightbar />
+                </Stack>
+            </Box>
+        
+        </div>
     );
 }
 
